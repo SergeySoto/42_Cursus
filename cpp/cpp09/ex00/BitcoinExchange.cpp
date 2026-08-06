a0696465df8955a9a404661a2e2e82d6588810f4
ssoto-su <ssoto-su@student.42malaga.com>
2026-08-06 18:07:04 +0200

    with cpp09

---
+++ cpp/cpp09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,89 @@
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() : mapBit[" "] = 0; {}
+
+bool BitcoinExchange::loadDataBase(const std::string &db) {
+	std::ifstream file(db.c_str());
+	if (!file.is_open()) {
+		std::cerr << "Error: could not open file." << std::endl;
+		return false;
+	}
+	std::string line;
+	std::getline(file, line);
+	while (std::getline(file, line)) {
+		std::string::size_type pos = line.find(',');
+		if (pos == std::string::npos)
+			continue ;
+		std::string date = line.substr(0, pos);
+		std::string flt = line.substr(pos + 1);
+		std::istringstream conversionStream(flt);
+		float num;
+		if (conversionStream >> num) {
+			this->mapBit[date] = num;
+		}
+	}
+	return true;
+}
+
+void BitcoinExchange::bitCoinCalculator(const std::string& file) {
+	std::ifstream fl(file.c_str());
+	if (!fl.is_open()) {
+		std::cerr << "Error: could not open file." << std::endl;
+		return ;
+	}
+	std::string line;
+	while (std::getline(fl, line)) {
+		std::string::size_type pos = line.find(" | ");
+		if (pos == std::string::npos)
+			continue ;
+		
+	}
+}
+
+bool BitcoinExchange::isValidDate(const std::string &date) {
+	std::string::size_type pos1 = date.find('-');
+	if (pos1 == std::string::npos) return false;
+	std::string stryear = date.substr(0, pos1);
+	std::string::size_type pos2 = date.find('-', pos1 + 1);
+	if (pos2 == std::string::npos) return false;
+	std::string strmonth = date.substr(pos1 + 1, pos2 - (pos1 + 1));
+	std::string strday = date.substr(pos2 + 1);
+	int year;
+	int month;
+	int day;
+	std::istringstream ssyear(stryear);
+	if (!(ssyear >> year) || !ssyear.eof()) return false;
+	std::istringstream ssmonth(strmonth);
+	if (!(ssmonth >> month) || !ssmonth.eof()) return false;
+	std::istringstream ssday(strday);
+	if (!(ssday >> day) || !ssday.eof()) return false;
+	if (month < 1 || month > 12) return false;
+	if (day < 1 || day > 31) return false;
+	if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) return false;
+	if (month == 2) {
+		bool isLeap = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+		if (isLeap) 
+			if (day > 29)
+				return false;
+		else
+			if (day > 28)
+				return false;
+	}
+	//if returns false it should print error message
+	return true;
+}
+
+bool BitcoinExchange::isValidValue(const std::string &valStr) {
+	float num;
+	std::istringstream ssnum(valStr);
+	if (!(ssnum >> num) || !ssnum.eof()) return false;
+	if (num < 0) {
+		std::cerr << "Error: not a positive number." << std::endl;
+		return false;
+	}
+	if (num > 1000) {
+		std::cerr << "Error: too large a number." << std::endl;
+		return false;
+	}
+	return true;
+}
---
+++ cpp/cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,25 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <map>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <limits>
+
+class BitcoinExchange {
+	private:
+		std::map <std::string, float> mapBit;
+		bool isValidDate(const std::string &date);
+		bool isValidValue(const std::string &valStr);
+	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange &copy);
+		BitcoinExchange& operator=(const BitcoinExchange &copy);
+		~BitcoinExchange();
+		bool loadDataBase(const std::string &db);
+		void bitCoinCalculator(const std::string &file);
+};
+
+#endif
