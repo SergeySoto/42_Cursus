3e1b4f7f95e9dbafbabd735a0b477baadf55aae1
ssoto-su <ssoto-su@student.42malaga.com>
2026-08-12 16:51:52 +0200

    ex01 almost finished. "./RPN    " needs to be fixed??

--- cpp/cpp09/ex01/RPN.cpp
+++ cpp/cpp09/ex01/RPN.cpp
@@ -15,15 +15,34 @@ RPN& RPN::operator=(const RPN &copy) {
 RPN::~RPN() {}
 
 void RPN::operation(const std::string &str) {
-	std::string list = "+-/*";
-	for (int c = 0; c != '\0'; ++c) {
-		if (c == ' ')
-			continue ;
-		if (std::isdigit(c)) //int num = c - '0';
-			this->lifo.push(c);
-		if (list.find(c) != std::string::npos) {
-			int val2 = this->lifo.top(); this->lifo.pop(); //switch?? think about it!
-			int val1 = this->lifo.top(); this->lifo.pop();
+	for (unsigned int i = 0; i < str.length(); ++i) {
+		char c = str[i];
+		if (c == ' ') continue ;
+		if (std::isdigit(c)) {
+			this->lifo.push(c - '0');
+			continue;
+		}
+		if (this->lifo.size() < 2){
+			std::cerr << "Error" << std::endl;
+			return;
+		}
+		int val2 = this->lifo.top(); this->lifo.pop();
+		int val1 = this->lifo.top(); this->lifo.pop();
+		switch (c) {
+			case '+': (this->lifo.push(val1 + val2)); break;
+			case '-': (this->lifo.push(val1 - val2)); break;
+			case '*': (this->lifo.push(val1 * val2)); break;
+			case '/': 
+				if (val2 == 0) {
+					std::cerr << "Error" << std::endl;
+					return;
+				}
+				(this->lifo.push(val1 / val2)); break;
+			default: std::cerr << "Error" << std::endl; return;
 		}
 	}
+	if (this->lifo.size() != 1)
+		std::cerr << "Error" << std::endl;
+	else
+		std::cout << this->lifo.top() << std::endl;
 }
---
+++ cpp/cpp09/ex01/main.cpp
@@ -0,0 +1,10 @@
+#include "RPN.hpp"
+
+int main (int ac, char**av) {
+	if (ac != 2)
+		std::cerr << "Error" << std::endl;
+	RPN op;
+
+	op.operation(av[1]);
+	return 0;
+}
