90dc95806ad05b17ab3f564d572325ef3dc88b68
ssoto-su <ssoto-su@student.42malaga.com>
2026-08-13 16:40:20 +0200

    finished ex01

--- cpp/cpp09/ex01/main.cpp
+++ cpp/cpp09/ex01/main.cpp
@@ -1,10 +1,11 @@
 #include "RPN.hpp"
 
 int main (int ac, char**av) {
-	if (ac != 2)
+	if (ac != 2){
 		std::cerr << "Error" << std::endl;
+		return 1;
+	}
 	RPN op;
-
 	op.operation(av[1]);
 	return 0;
 }
